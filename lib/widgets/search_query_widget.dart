d1d9ecb7e9ab48c52337b5c5056f426f19e44c88
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-07-12 22:52:48 +0100

    You can now select a path from the file finder menu rather than having to type it

--- lib/widgets/search_query_widget.dart
+++ lib/widgets/search_query_widget.dart
@@ -1,4 +1,5 @@
 import 'package:file_finder/models/file_search_request.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class SearchQueryWidget extends StatefulWidget {
@@ -12,6 +13,7 @@ class _SearchQueryState extends State<SearchQueryWidget> {
 
   final FileSearchRequest _request = FileSearchRequest();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _pathFormFieldController = TextEditingController(text: '');
 
   @override
   Widget build(BuildContext context) {
@@ -45,18 +47,30 @@ class _SearchQueryState extends State<SearchQueryWidget> {
                         )
                       ),
                       const Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                      Flexible(
-                          child: TextFormField(
-                            validator: (value) => _validateFormField(value, 'Please enter a search query'),
-                            decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Enter the path you want to query (e.g. C:\\User\\Documents)',
-                                labelText: 'Path'
-                            ),
-                            onSaved: (String? value) {
-                              _request.basePath = value!;
-                            },
-                          )
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () => selectDirectoryToSearch(),
+                              child: const Text('Select Path')
+                          ),
+                          const Padding(padding: EdgeInsets.only(right: 16.0)),
+                          Flexible(
+                            child: TextFormField(
+                              controller: _pathFormFieldController,
+                              validator: (value) => _validateFormField(value, 'Please enter a path to search in'),
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter the path you want to query (e.g. C:\\User\\Documents)',
+                                  labelText: 'Path'
+                              ),
+                              onSaved: (String? value) {
+                                _request.basePath = value!;
+                              },
+                            )
+                          ),
+                        ],
                       ),
                       const Padding(padding: EdgeInsets.only(bottom: 16.0)),
                       ElevatedButton(
@@ -74,8 +88,25 @@ class _SearchQueryState extends State<SearchQueryWidget> {
     );
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _pathFormFieldController.dispose();
+  }
+
   void submitForm() {
 
+  }
+
+  void selectDirectoryToSearch() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+
+    if(selectedDirectory == null) {
+      return;
+    }
+
+    _request.basePath = selectedDirectory;
+    _pathFormFieldController.text = selectedDirectory;
   }
 
   String? _validateFormField(value, String errorMessage) {
